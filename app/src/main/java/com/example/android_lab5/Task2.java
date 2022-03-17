00a317f36d43fc9fd37f919fcc772eb8a22fadd4
Alexandr <minisam2468@gmail.com>
2022-03-18 04:26:28 +0600

    patch Task2

--- app/src/main/java/com/example/android_lab5/Task2.java
+++ app/src/main/java/com/example/android_lab5/Task2.java
@@ -1,9 +1,19 @@
 package com.example.android_lab5;
 
-import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.ArrayList;
 
 public class Task2 extends AppCompatActivity {
+    private Button btn, btn2;
+    private EditText editText1, editText2;
+    private TextView textView, textView2;
+    private final ArrayList<String> strings = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,5 +23,34 @@ public class Task2 extends AppCompatActivity {
     }
 
     private void addListenerOnButton() {
+        btn = (Button) findViewById(R.id.task2button);
+        btn2 = (Button) findViewById(R.id.task2button2);
+        editText1 = (EditText) findViewById(R.id.task2editText);
+        editText2 = (EditText) findViewById(R.id.task2editText2);
+        textView = (TextView) findViewById(R.id.task2textView);
+        textView2 = (TextView) findViewById(R.id.task2textView2);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String s = String.valueOf(editText1.getText());
+                strings.add(s);
+                editText1.setText("");
+                textView2.setText(strings.toString());
+            }
+        });
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String input = String.valueOf(editText2.getText());
+                boolean output = false;
+                for (String string : strings) {
+                    if (string.equals(input)) {
+                        output = true;
+                        break;
+                    }
+                }
+                textView.setText(String.valueOf(output));
+            }
+        });
     }
 }
